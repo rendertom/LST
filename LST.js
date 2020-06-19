be7006f8130927a562589c06dbc4d0ae18258121
Tomas Sinkunas <Tomas@rendertom.com>
2020-06-19 20:58:30 +0300

    initial commit

---
+++ LST.js
@@ -0,0 +1,171 @@
+var LST = (function() {
+	// @include 'lib/CameraEx.js'
+	// @include 'lib/CompositionEx.js'
+	// @include 'lib/LayerEx.js'
+	// @include 'lib/MathEx.js'
+	// @include 'lib/Matrix.js'
+	// @include 'lib/Trig.js'
+	// @include 'lib/Vector3.js'
+
+
+	var module = {};
+	/**
+	 * Transforms a point from layer scope into composition scope.
+	 * @param  {LayerObject} 	layer  	Layer
+	 * @param  {Array} 			offset 	Point coordinates, optional. Defaults to layers anchor point value.
+	 * @return {Array}        			Points coordinates in composition scope.
+	 */
+	module.toComp = function(layer, offset) {
+		offset = fixOffset(layer, offset);
+
+		var result;
+		if (!layer.threeDLayer) {
+			result = toComp2D(layer, offset);
+			result.pop();
+		} else {
+			result = toComp3D(layer, offset);
+		}
+
+		return result;
+	};
+
+	/**
+	 * Transforms a point from layer scope into view-independent world scope.
+	 * @param  {LayerObject} 	layer  	Layer
+	 * @param  {Array} 			offset 	Point coordinates, optional. Defaults to layers anchor point value.
+	 * @return {Array}        			Points coordinates in composition scope.
+	 */
+	module.toWorld = function(layer, offset) {
+		offset = fixOffset(layer, offset);
+
+		var modelMatrix = getModelMatrix(layer, offset);
+		var result = Matrix.getTranslate(modelMatrix);
+		result[2] *= -1;
+
+		if (!layer.threeDLayer) {
+			result.pop();
+		}
+
+		return result;
+	};
+
+	return module;
+
+
+
+	function fixOffset(layer, offset) {
+		var property, value;
+
+		if (typeof offset === 'undefined') {
+			offset = [0, 0, 0];
+		} else {
+			property = layer.property('ADBE Transform Group').property('ADBE Anchor Point');
+			value = property.value;
+
+			value[2] *= -1;
+			offset -= value;
+		}
+
+		return offset;
+	}
+
+	function getModelMatrix(layer, offset) {
+		var layerLocalMatrix = LayerEx.getLocalMatrix(layer);
+		var layerWorldMatrix = LayerEx.getWorldMatrix(layer);
+		var offsetMatrix = getOffsetMatrix(offset);
+
+		var modelMatrix = Matrix.multiplyArrayOfMatrices([
+			layerWorldMatrix,
+			layerLocalMatrix,
+			offsetMatrix
+		]);
+
+		return modelMatrix;
+	}
+
+	function getOffsetMatrix(offset) {
+		var matrix = Matrix.getIdentity();
+		if (typeof offset !== 'undefined') {
+			matrix = Matrix.translate(matrix, offset[0], offset[1], offset[2]);
+		}
+
+		return matrix;
+	}
+
+	function getProjectedZ(composition) {
+		var camera, z;
+
+		camera = composition.activeCamera;
+		if (camera && camera.enabled) {
+			z = CameraEx.getProjectedZ(camera);
+		} else {
+			z = CompositionEx.getProjectedZ(composition);
+		}
+
+		return z;
+	}
+
+	function getProjectionMatrix(composition) {
+		var projectionMatrix = Matrix.perspective(
+			getAOV(composition),
+			composition.width / composition.height,
+			0.1, 10000
+		);
+
+		return projectionMatrix;
+
+		function getAOV(composition) {
+			var camera = composition.activeCamera;
+			if (camera && camera.enabled) {
+				return CameraEx.getAOV(camera);
+			} else {
+				return CompositionEx.getAOV();
+			}
+		}
+	}
+
+	function getViewMatrix(composition) {
+		var camera, viewMatrix;
+
+		camera = composition.activeCamera;
+		if (camera && camera.enabled) {
+			viewMatrix = CameraEx.getViewMatrix(camera);
+		} else {
+			viewMatrix = CompositionEx.getViewMatrix(composition);
+		}
+
+		return viewMatrix;
+	}
+
+	function toComp2D(layer, offset) {
+		var modelMatrix = getModelMatrix(layer, offset);
+		return Matrix.getTranslate(modelMatrix);
+	}
+
+	function toComp3D(layer, offset) {
+		var modelMatrix = getModelMatrix(layer, offset);
+		var viewMatrix = getViewMatrix(layer.containingComp);
+		var projectionMatrix = getProjectionMatrix(layer.containingComp);
+
+		var mvp = Matrix.multiplyArrayOfMatrices([
+			projectionMatrix,
+			Matrix.invert(viewMatrix),
+			modelMatrix,
+		]);
+
+		return toScreenCoordinates(mvp, layer.containingComp);
+	}
+
+	function toScreenCoordinates(mvp, composition) {
+		var ndc, x, y, z, w;
+
+		w = mvp[15];
+		ndc = Matrix.getTranslate(mvp) / w;
+
+		x = (ndc[0] + 1) * composition.width / 2;
+		y = (ndc[1] + 1) * composition.height / 2;
+		z = getProjectedZ(composition);
+
+		return [x, y, z];
+	}
+})();
--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# LST
-Layer Space Transforms
+# LST #
+
+LST, short for Layer Space Transforms, is a utility class for Adobe After Effects, that helps calculating layers position in composition scope, very similar to `toWorld()` and `toComp()` methods available inside expressions.
+
+## API ##
+
+* `LST.toComp(layer, offset);` Transforms a point from layer scope into composition scope.
+* `LST.toWorld(layer, offset);` Transforms a point from layer scope into view-independent world scope.
+
+## Usage ##
+
+Download the repository and include the class in your main script.
+```javascript
+// Include the class
+#include 'LST.js'
+```
+
+### Case 1 ###
+
+```javascript
+// Get layers position in composition scope
+LST.toComp(layer);
+```
+
+### Case 2 ###
+
+```javascript
+// Get layers top-left point coordinate in composition scope
+LST.toComp(layer, [0, 0, 0]);
+```
+
+### Case 3 ###
+
+```javascript
+// Get layers position in composition scope and ignore active composition camera
+LST.toWorld(layer);
+```
+---
+
+## Notes ##
+
+There's a slight Z-position error (0.0 <= 0.3) when running `toComp()` method on a 3D layer in the composition with a camera. I wasn't able to find a more robust way to calculate a projected Z position, sorry.
+
+LST assumes that the default **FILM SIZE** value is **36.00mm**, as there's no API in After Effects to retrieve this value. Also, when processing a 3D layer inside a composition that contains no active camera, the **FOCAL LENGTH** defaults to **50.00mm** (again, lack of API on AE's side).
+
+---
+
+## Todo ##
+* `fromComp()`: Transform a point from composition scope inco layer scope. 
+* `fromWorld()`: Transform a point from world space to layer space.
+
+---
+
+## Resources ##
+* Three.js: https://github.com/mrdoob/three.js
+* Interactive 3D Graphics course on Udacity: https://classroom.udacity.com/courses/cs291
+* ModelViewProjection live demo: http://www.realtimerendering.com/udacity/transforms.html
+* Foundations of 3D Rendering on Scratchapixel 2.0: https://www.scratchapixel.com/
+* 3D Game Engine Programming: https://www.3dgep.com/understanding-the-view-matrix/
+* World, View and Projection Transformation Matrices on Codinglabs: http://www.codinglabs.net/article_world_view_projection_matrix.aspx
